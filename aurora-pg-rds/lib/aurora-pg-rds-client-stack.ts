e221931ef3a8b0c780c583cc39048d1caa54e31f
aaronw <aaronw@wsu.edu>
2023-06-02 09:22:15 -0700

    Update aurora to use CDK 2.82.0 and new serverless2 construct

--- aurora-pg-rds/lib/aurora-pg-rds-client-stack.ts
+++ aurora-pg-rds/lib/aurora-pg-rds-client-stack.ts
@@ -13,6 +13,10 @@ export class AuroraPgClientStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: AuroraPgClientStackProps) {
     super(scope, id, props);
 
+    if (process.env.VPC_ID == null) {
+      console.log("environment variable VPC_ID must be set to desired VPC")
+      process.exit(1)
+    }
     const vpc = ec2.Vpc.fromLookup(this,'vpc',{ vpcId: process.env.VPC_ID })
     const subnets = vpc.selectSubnets( { subnetType: ec2.SubnetType.PRIVATE_ISOLATED } );
 
--- aurora-pg-rds/lib/aurora-pg-rds-stack.ts
+++ aurora-pg-rds/lib/aurora-pg-rds-stack.ts
@@ -3,7 +3,7 @@ import { Construct } from 'constructs';
 import * as secrets from 'aws-cdk-lib/aws-secretsmanager';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as autoscale from 'aws-cdk-lib/aws-autoscaling';
+import * as logs from 'aws-cdk-lib/aws-logs';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 // in addition to the standard stack properties, we want a list of networks that can connect to the service, and the vpc to deploy into.
@@ -27,6 +27,10 @@ export class AuroraPgRdsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: AuroraPgRdsStackProps) {
     super(scope, id, props);
 
+    if (process.env.VPC_ID == null) {
+      console.log("environment variable VPC_ID must be set to desired VPC")
+      process.exit(1)
+    }
     const vpc    = ec2.Vpc.fromLookup(this,'default_vpc',{ vpcId: process.env.VPC_ID })
 
     // first, lets generate a secret to be used as credentials for our database
@@ -39,39 +43,57 @@ export class AuroraPgRdsStack extends cdk.Stack {
         excludePunctuation: true,
         includeSpace: false,
         generateStringKey: 'password'
-      }
+      },
     });
 
+    // DatabaseCluster supports Aurora V2 as of CDK 2.82.0
     // the ServerlessCluster construct creates a v1 serverless cluster. There's no support for V2 directly, but the
     // workaround is to create a DatabaseCluster and override the instance-related structures with serverless.
     // See https://github.com/aws/aws-cdk/issues/20197
+    const parameterGroup = rds.ParameterGroup.fromParameterGroupName(this,'auroraPgGroup','default.aurora-postgresql15')
     this.rds = new rds.DatabaseCluster(this,"auroradb",{
       engine: rds.DatabaseClusterEngine.auroraPostgres({
         version: rds.AuroraPostgresEngineVersion.VER_15_2
       }),
-      instanceProps: {
-        vpc: vpc,
-        instanceType: "serverless" as any,
-        vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
-      },
       credentials: rds.Credentials.fromSecret(databaseCredentialsSecret),
       defaultDatabaseName: 'mydata',
       copyTagsToSnapshot: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       iamAuthentication: false,
       storageEncrypted: true,
-      instances: 1,
+      instanceUpdateBehaviour: rds.InstanceUpdateBehaviour.ROLLING,
       backup: {
         retention: cdk.Duration.days(5)
       },
-      parameterGroup: rds.ParameterGroup.fromParameterGroupName(this,'auroraPgGroup','default.aurora-postgresql15')
+      parameterGroup: parameterGroup,
+      cloudwatchLogsExports: ['postgresql'], // Export all available MySQL-based logs
+      cloudwatchLogsRetention: logs.RetentionDays.THREE_MONTHS, // Optional - default is to never expire logs
+      readers: [
+        rds.ClusterInstance.serverlessV2('reader1',{scaleWithWriter: true })
+      ],
+      writer:
+        rds.ClusterInstance.serverlessV2('writer',{
+          publiclyAccessible: true,
+          parameterGroup: parameterGroup}),
+      serverlessV2MaxCapacity: 2,
+      serverlessV2MinCapacity: 1,
+      vpc: vpc,
+      vpcSubnets:  { subnetType: ec2.SubnetType.PUBLIC },
     });
-    // add a scaling configuration to the raw Cfn object.
-    (this.rds.node.findChild("Resource") as rds.CfnDBCluster).serverlessV2ScalingConfiguration = {
-      minCapacity: 1,
-      maxCapacity: 2,
-    }
-
+/*
+    this.rds.metricServerlessDatabaseCapacity({
+      period: cdk.Duration.minutes(10),
+    }).createAlarm(this, 'capacity', {
+        threshold: 1.5,
+        evaluationPeriods: 3,
+    });
+    this.rds.metricACUUtilization({
+      period: cdk.Duration.minutes(10),
+    }).createAlarm(this, 'alarm', {
+      evaluationPeriods: 3,
+      threshold: 90,
+    });
+*/
     // allow access to the Aurora cluster from outside networks.
     props.allowedNetworks.forEach( (network) => {
       this.rds.connections.allowFrom(ec2.Peer.ipv4(network['cidr']),ec2.Port.tcp(5432),network['comment'])
